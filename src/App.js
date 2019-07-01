fb26b5ef80e5e0f3fa11668070004266145bc3e9
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-07-01 11:06:29 -0400

    made basic stopwatch and timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+//import libraries
+import React, {
+  Component
+} from 'react';
+
+//import Components
+import Button from './button'
+//import CSS
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    timer: 0,
+    countdown: 10
+  }
+
+  countDownInterval = 0;
+
+  addTime = () => {
+    let newTime = this.state.timer + 1;
+    this.setState({
+      timer: newTime
+    });
+  };
+
+  startStopwatch = () => {
+    this.countDownInterval = setInterval(this.addTime, 1000);
+  };
+
+  stopStopwatch = () => {
+    clearInterval(this.countDownInterval);
+  };
+
+  resetStopwatch = () => {
+    clearInterval(this.countDownInterval);
+    this.setState({
+      timer: 0
+    });
+  };
+
+  minusTime = () => {
+    let newTime = this.state.countdown -1;
+    this.setState({
+      countdown: newTime
+    });
+  };
+
+  startTimer = () => {
+    this.countDownInterval = setInterval(this.minusTime, 1000);
+  };
+
+  stopTimer = () => {
+    clearInterval(this.countDownInterval);
+  };
+
+  resetTimer= () => {
+    clearInterval(this.countDownInterval);
+    this.setState({
+      countdown: 10
+    });
+  }
+
+  render() {
+    const {
+      timer, countdown
+    } = this.state;
+    return ( 
+      <div className="App">
+      <p> Stopwatch: {timer} </p> 
+      <Button clickHandler={this.startStopwatch} name={'Start'}/> 
+      <Button clickHandler={this.stopStopwatch} name={'Stop'}/> 
+      <Button clickHandler={this.resetStopwatch} name={'Reset'}/>
+      <p>Timer: {countdown}</p>
+      <Button clickHandler={this.startTimer} name={'Start'}/>
+      <Button clickHandler={this.stopTimer} name={'Stop'}/> 
+      <Button clickHandler={this.resetTimer} name={'Reset'}/>
+      </div>
+    );
+  };
+};
 
 export default App;
